9c4e75d03a53ce28aa6997fa47c9020135baf8cd
Cansel Canayakın <canayakincansel@gmail.com>
2018-10-11 14:35:14 +0300

    added quick sort method

--- src/Main.java
+++ src/Main.java
@@ -21,6 +21,12 @@ public class Main {
         for(int b = 0;b<a.length;b++)
             System.out.print(a[b]+" ");
         System.out.println();
+
+        sort(a,0,12);
+        System.out.print("Quick sort: ");
+        for(int b = 0;b<a.length;b++)
+            System.out.print(a[b]+" ");
+        System.out.println();
     }
 
     public static void SelectionSort (int[] arr){
@@ -67,11 +73,51 @@ public class Main {
 
     }
 
-   /* public static void quicksort(int[] arr){
+    static int partition(int arr[], int low, int high)
+    {
+        int pivot = arr[high];
+        int i = (low-1); // index of smaller element
+        for (int j=low; j<high; j++)
+        {
+            // If current element is smaller than or
+            // equal to pivot
+            if (arr[j] <= pivot)
+            {
+                i++;
 
+                // swap arr[i] and arr[j]
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
 
+        // swap arr[i+1] and arr[high] (or pivot)
+        int temp = arr[i+1];
+        arr[i+1] = arr[high];
+        arr[high] = temp;
+
+        return i+1;
     }
 
-    public static void parca() */
+
+    /* The main function that implements QuickSort()
+      arr[] --> Array to be sorted,
+      low  --> Starting index,
+      high  --> Ending index */
+    static void sort(int arr[], int low, int high)
+    {
+        if (low < high)
+        {
+            /* pi is partitioning index, arr[pi] is
+              now at right place */
+            int pi = partition(arr, low, high);
+
+            // Recursively sort elements before
+            // partition and after partition
+            sort(arr, low, pi-1);
+            sort(arr, pi+1, high);
+        }
+    }
 
 }
